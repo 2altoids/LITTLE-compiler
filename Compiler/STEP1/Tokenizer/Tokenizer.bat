c58c237c250795f22d81c92e086fc21b5047027f
Alexandr <alex.matveyev88@gmail.com>
2019-02-17 10:34:35 -0700

    STEP1 and STEP2
    The Compiler folder has STEP1 and STEP2 with batch files

---
+++ Compiler/STEP1/Tokenizer/Tokenizer.bat
@@ -0,0 +1,5 @@
+java org.antlr.v4.Tool MicroGrammar.g4
+mkdir classes
+javac -d classes/ MicroGrammar*.java
+javac -d classes/ Main.java
+javac -d classes/ CustomToken.java
---
+++ Compiler/STEP2/Parser/Parser.bat
@@ -0,0 +1,6 @@
+java org.antlr.v4.Tool MicroGrammar.g4
+mkdir classes
+javac -d classes/ MicroGrammar*.java
+javac -d classes/ Main.java
+javac -d classes/ CustomToken.java
+javac -d classes/ MicroParser.java
