006bd77c3ec02c9659f0427a87e5e79fa328cecd
Alexandr <alex.matveyev88@gmail.com>
2019-02-22 21:45:42 -0700

    STEP1 and STEP2 updated

--- Compiler/STEP1/Tokenizer/Micro2.sh
+++ Compiler/STEP1/Tokenizer/Micro2.sh
@@ -2,7 +2,7 @@
 SCRIPTDIR=`cd "$(dirname "$0")" && pwd`
 java org.antlr.v4.Tool "$SCRIPTDIR/MicroGrammar.g4"
 javac MicroGrammar*.java
-javac Main.java
 javac CustomToken.java
+javac Main.java
 args=("$@")
 java Main $@
--- Compiler/STEP2/Parser/Micro2.sh
+++ Compiler/STEP2/Parser/Micro2.sh
@@ -2,7 +2,8 @@
 SCRIPTDIR=`cd "$(dirname "$0")" && pwd`
 java org.antlr.v4.Tool "$SCRIPTDIR/MicroGrammar.g4"
 javac MicroGrammar*.java
-javac Main.java
 javac CustomToken.java
+javac MicroParser.java
+javac Main.java
 args=("$@")
 java Main $@
